1f706d634a73205ede0fd34f17ddf57b363712a4
imztone <mansonzzz11@gmail.com>
2025-03-27 09:28:41 +0800

    first commit

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://run.imzt.one',
+  logo: 'https://avatars.githubusercontent.com/u/33047106?v=4',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Github',
+      url: 'https://github.com/imztone',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/imztone',
+    },
+  ],
+};
+
+export default data;
